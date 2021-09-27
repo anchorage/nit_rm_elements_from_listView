3f6ca4a7402e5313ab2dcaff960a5b7b7a6e9b9b
anchorage <anchorage@github>
2021-09-27 23:21:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.company.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var demoList = MutableList(11) { x -> (x * x).toString() }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, demoList)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            demoList.remove((view as TextView).text.toString())
+            adapter.notifyDataSetChanged()
+
+
+        }
+
+    }
+}
